5de70b080101717cc021e442eb49ebb9878152bc
2wndrhs <2wndrhs@gmail.com>
2022-12-06 19:02:54 +0900

    test(lotto): Validation 테스트 코드 작성 로또 구입 금액이 1000원 단위의 숫자가 아니면 예외 발생하는지 확인하는 테스트 작성

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -2,7 +2,7 @@ const { LOTTO_BASE } = require('./constants');
 
 class Validator {
   static #ERROR_MESSAGE = Object.freeze({
-    isPurchaseInput: `로또 구입 금액은 ${LOTTO_BASE.PRICE}원 단위의 숫자입니다.`,
+    isPurchaseAmount: `로또 구입 금액은 ${LOTTO_BASE.PRICE}원 단위의 숫자입니다.`,
   });
 
   static validate(input, validator) {
@@ -11,10 +11,10 @@ class Validator {
     }
   }
 
-  static isPurchaseInput(input) {
-    const NUMBER_INPUT_PATTERN = /^[^0]\d+$/;
+  static isPurchaseAmount(input) {
+    const PURCHASE_AMOUNT_PATTERN = /^[^0]\d+$/;
 
-    return NUMBER_INPUT_PATTERN.test(input) && !(Number(input) % LOTTO_BASE.PRICE);
+    return PURCHASE_AMOUNT_PATTERN.test(input) && !(Number(input) % LOTTO_BASE.PRICE);
   }
 }
 
